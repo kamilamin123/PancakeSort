1aee0e50ee49f3b04e6056f73ab5ef5f5bc8258c
kamil_Amin <kamil_1@hotmail.co.uk>
2023-02-16 22:42:26 -0500

    1.5

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -3,48 +3,43 @@ package main;
 import java.util.Arrays;
 
 class Solution {
-    public static void pancakeSort(int[] inputArray) {
-    	
-    	for (int i=inputArray.length; i>=1; i--) {
-    		int maxIndex=find(inputArray,i);
-    		if (maxIndex!=i-1) {
-    			 
-    			flip(inputArray,maxIndex);
-    			System.out.println(Arrays.toString(inputArray));
-    			flip(inputArray,i-1);
-    			 System.out.println(Arrays.toString(inputArray));
-    		}
-    	}
-    }
-    
-    private static int find(int[] a, int target) {
-    	int maxInd=0;
-    	for (int i=1; i<target; i++) {
-    	  if(a[maxInd]<=a[i]) {
-    		  maxInd=i;
-    	  }
-    	  
-      }
-    	return maxInd;
-    }
-    private static void flip(int[] a, int index) {
-    	int temp;
-    	for (int i=0; i<=index-i; i++) {
-    		temp=a[i];
-    		a[i]=a[index-i];
-    		a[index-i]=temp;
-    		
-    		}
-    }
-//start
-    public static void main(String[] args){
-        int[] arr = {7,2,5,23,21,38,1,7,8,9,10,20,200,300};
-    
-        pancakeSort(arr);
-        for(int a:arr) {
-        	System.out.print(a+", ");
-        }
-       
-    
-    }
+	public static void pancakeSort(int[] inputArray) {
+
+		for (int i=inputArray.length; i>=1; i--) {
+			int maxIndex=find(inputArray,i);
+			if (maxIndex!=i-1) { 
+				flip(inputArray,maxIndex);
+				flip(inputArray,i-1);
+			}
+		}
+	}
+
+	private static int find(int[] a, int target) {
+		int maxInd=0;
+		for (int i=1; i<target; i++) {
+			if(a[maxInd]<=a[i]) {
+				maxInd=i;
+			}
+
+		}
+		return maxInd;
+	}
+	private static void flip(int[] a, int index) {
+		int temp;
+		for (int i=0; i<=index-i; i++) {
+			temp=a[i];
+			a[i]=a[index-i];
+			a[index-i]=temp;
+
+		}
+	}
+	//start
+	public static void main(String[] args){
+		int[] arr = {7,2,5,23,21,38,1,7,8,9,10,20,200,300};
+
+		pancakeSort(arr);
+			System.out.println(Arrays.toString(arr));
+
+
+	}
 }
